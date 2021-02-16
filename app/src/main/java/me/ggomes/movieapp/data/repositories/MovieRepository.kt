d8b35dff85050635a36a2030788741181d1b98b0
Gustavo Gomes <gfrgomes4@gmail.com>
2021-02-16 16:11:37 +0100

    Absorbing network errors if DB has data

--- app/src/main/java/me/ggomes/movieapp/data/repositories/MovieRepository.kt
+++ app/src/main/java/me/ggomes/movieapp/data/repositories/MovieRepository.kt
@@ -26,18 +26,24 @@ class MovieRepository(
         if (databaseResults.isNotEmpty())
             emit(databaseResults)
 
-        val response = apiService.search(term)
-        val movies = response.search.map {
-            Movie(it.imdbId, it.title, it.year, it.posterUrl)
-        }.distinct()
+        try {
+            val response = apiService.search(term)
+            val movies = response.search.map {
+                Movie(it.imdbId, it.title, it.year, it.posterUrl)
+            }.distinct()
 
-        if (movies != databaseResults) {
-            emit(movies)
+            if (movies != databaseResults) {
+                emit(movies)
 
-            movies.forEach { movie ->
-                if (!databaseResults.contains(movie))
-                    database.movieDao().insert(movie)
+                movies.forEach { movie ->
+                    if (!databaseResults.contains(movie))
+                        database.movieDao().insert(movie)
+                }
             }
+        } catch (exception: Exception) {
+            // Don't absorb exception if Database didn't provide results
+            if (databaseResults.isEmpty())
+                throw exception
         }
 
     }
@@ -48,21 +54,27 @@ class MovieRepository(
             emit(it)
         }
 
-        val networkResponse = apiService.getMovieBy(movieId)
-        val movie = MovieDetail(
-                movieId,
-                networkResponse.title,
-                networkResponse.posterUrl,
-                networkResponse.plot,
-                networkResponse.year,
-                networkResponse.actors,
-                networkResponse.director
-        )
+        try {
+            val networkResponse = apiService.getMovieBy(movieId)
+            val movie = MovieDetail(
+                    movieId,
+                    networkResponse.title,
+                    networkResponse.posterUrl,
+                    networkResponse.plot,
+                    networkResponse.year,
+                    networkResponse.actors,
+                    networkResponse.director
+            )
 
-        if (databaseResult != movie) {
-            database.movieDetailDao().delete(movieId)
-            database.movieDetailDao().insert(movie)
-            emit(movie)
+            if (databaseResult != movie) {
+                database.movieDetailDao().delete(movieId)
+                database.movieDetailDao().insert(movie)
+                emit(movie)
+            }
+        } catch (exception: Exception) {
+            // Don't absorb exception if Database didn't provide results
+            if (databaseResult == null)
+                throw exception
         }
     }
 }
